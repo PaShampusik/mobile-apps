048762cdbffd25a0dad763865b8a1697acbfd6e9
Shchirov Pavel <shchirovpavel@gmail.com>
2024-04-12 12:35:22 +0300

    Lab2 done

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,17 +1,60 @@
 package com.example.calculator
 
-import net.objecthunter.exp4j.ExpressionBuilder
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import net.objecthunter.exp4j.ExpressionBuilder
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+
+    private lateinit var result: TextView
+    private lateinit var operation: TextView
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+
+            val res: TextView = findViewById<TextView>(R.id.result)
+            val op: TextView = findViewById<TextView>(R.id.operation)
+
+            // Fetching x,y,z values
+            val x = event.values[0]
+            val y = event.values[1]
+            lastAcceleration = currentAcceleration
+
+            // Getting current accelerations
+            currentAcceleration = sqrt((x * x + y * y).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.8f + delta
+
+            if (acceleration > 8) {
+                result.text = ""
+                operation.text = ""
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val result: TextView = findViewById<TextView>(R.id.result)
-        val operation: TextView = findViewById<TextView>(R.id.operation)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        result = findViewById<TextView>(R.id.result)
+        operation = findViewById<TextView>(R.id.operation)
 
         val sqrt: TextView = findViewById<TextView>(R.id.b_sqrt)
         val log2: TextView = findViewById<TextView>(R.id.b_log2)
@@ -117,5 +160,18 @@ class MainActivity : AppCompatActivity() {
                 result.text = ""
             }
         }
+    }
+
+    override fun onResume() {
+        sensorManager?.registerListener(
+            sensorListener, sensorManager!!.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER
+            ), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
     }
 }
